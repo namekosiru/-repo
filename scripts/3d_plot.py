39addc1cb836fbd6260a35b7b68e8537e6d64a11
namekosiru <rinjyou0031@toki.waseda.jp>
2021-09-11 13:57:07 +0900

    クラスの追加

--- scripts/3d_plot.py
+++ scripts/3d_plot.py
@@ -5,55 +5,45 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import ConvexHull
 import cv2
 
+class Plot_Utils():
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.xy = [[i, j] for i, j in zip(self.x, self.y)]
+        self.hull = ConvexHull(self.xy)
+
+    def plot_3d(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.set_title("3D plot")
+        ax.set_xlabel("x [cm]")
+        ax.set_ylabel("y [cm]")
+        ax.set_zlabel("z [cm]")
+        ax.scatter(self.x, self.y, self.z, marker="^")
+
+    def plot_2d(self):
+        points = self.hull.points
+        hull_points = points[self.hull.vertices]
+        hp = np.vstack((hull_points, hull_points[0]))
+        print(f"area : {self._polygon_area(hp[:,0], hp[:,1])} [cm]")       
+
+        fig = plt.figure()
+        plt.scatter(self.x, self.y, label="key points")
+        plt.xlabel("X [cm]")
+        plt.ylabel("Y [cm]")
+        plt.title("area")
+        plt.plot(hp[:,0], hp[:,1], c="green", label="outline")
+        plt.legend()     
+
+    def _polygon_area(self, x, y):
+        return abs(sum(x[i]*y[i-1] - y[i]*x[i-1] for i in range(len(x)))) / 2
+
 def get_u_v(x, y, z, L):
     u = (x * L[0] + y * L[1] + z * L[2] + L[3]) / (x * L[8] + y * L[9] + z * L[10] + 1)
     v = (x * L[4] + y * L[5] + z * L[6] + L[7]) / (x * L[8] + y * L[9] + z * L[10] + 1)
 
-def polygon_area(x, y):
-    return abs(sum(x[i]*y[i-1] - y[i]*x[i-1] for i in range(len(x)))) / 2
 
-# def calc_area(x, y):
-#     area = 0
-#     for i in range(len(x)):
-#         area += abs(x[i] * y[i-1] - x[i-1] * y[i])
-#     return 0.5 * area
-
-def create_convex_map(X, Y):
-    xy_lis = []
-    for x, y in zip(X, Y):
-        xy_lis.append([x,y])
-    xy_lis = np.array(xy_lis)
-    hull = ConvexHull(xy_lis)
-
-    #外周部分の座標配列
-    # x, y = [], []
-    # simplex:array配列の結合部分のindex番号
-    # for simplex, vertice in zip(hull.simplices, hull.vertices):
-    #     x.append(xy_lis[vertice][0])
-    #     y.append(xy_lis[vertice][1])
-    #     plt.plot(xy_lis[simplex, 0], xy_lis[simplex, 1], c="green")
-
-
-    return hull.simplices, hull.vertices
-
-def plot_3d(x, y, z):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.set_title("3D plot")
-    ax.set_xlabel("x [cm]")
-    ax.set_ylabel("y [cm]")
-    ax.set_zlabel("z [cm]")
-    ax.scatter(x, y, z, marker="^")
-
-def plot_2d(x, y, simplices=None, vertices=None):
-    fig = plt.figure()
-    plt.scatter(x, y, label="key points")
-    plt.xlabel("X [cm]")
-    plt.ylabel("Y [cm]")
-    plt.title("area ")
-    for simplex, vertice in zip(simplices, vertices):
-        plt.plot(x[simplex], y[simplex], c="green")
-    print(f"area : {polygon_area(x, y)} [cm]")
 
 if __name__ == "__main__":
     X_color_M, uv_color = get_X_matrix_and_uv_list("color")
@@ -61,10 +51,10 @@ if __name__ == "__main__":
     L1 = get_dlt_parameter(X_color_M, uv_color)
     L2 = get_dlt_parameter(X_gray_M, uv_gray)
 
-    data = pd.read_csv("./data/pair_plot_position/all_keypoints.txt", header=None)
     xyz3s = np.load('./data/np_xyz.npy')
     x_kin, y_kin, z_kin = xyz3s[:,0] * 100 , xyz3s[:,1] * 100, xyz3s[:,2] * 100 # kinectから読み込み cm→m単位
 
+    data = pd.read_csv("./data/pair_plot_position/all_keypoints.txt", header=None)
     X_dlt, Y_dlt, Z_dlt = [], [], []
     for _, content in data.iterrows(): #dltから読み込み　cm単位
         position = list(map(int, content[0].split(" ")))
@@ -73,10 +63,9 @@ if __name__ == "__main__":
         Y_dlt.append(y)
         Z_dlt.append(-z)
 
-    # plot_3d(X_dlt, Y_dlt, Z_dlt)
-    plot_3d(x_kin, y_kin, z_kin)
-    sim, con = create_convex_map(x_kin, y_kin)
-    # create_convex_map(X_dlt, Y_dlt)#DLTから算出したもの
-    plot_2d(x_kin, y_kin, sim, con)#Kinectから
+    kinect = Plot_Utils(x_kin, y_kin, z_kin)
+    kinect.plot_3d()
+    kinect.plot_2d()
+    # dlt = Plot_Utils(X_dlt, Y_dlt, Z_dlt)
 
     plt.show()
