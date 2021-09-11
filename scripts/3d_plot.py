946366c6c8efc2640ef62497a61c0880633dfc43
namekosiru <rinjyou0031@toki.waseda.jp>
2021-09-11 12:56:09 +0900

    リファクタリング

--- scripts/3d_plot.py
+++ scripts/3d_plot.py
@@ -10,7 +10,7 @@ def get_u_v(x, y, z, L):
     v = (x * L[4] + y * L[5] + z * L[6] + L[7]) / (x * L[8] + y * L[9] + z * L[10] + 1)
 
 def polygon_area(x, y):
-    return abs(sum(x[i]*y[i-1] - y[i]*x[i-1] for i in range(len(x)))) / 2.
+    return abs(sum(x[i]*y[i-1] - y[i]*x[i-1] for i in range(len(x)))) / 2
 
 # def calc_area(x, y):
 #     area = 0
@@ -19,26 +19,41 @@ def polygon_area(x, y):
 #     return 0.5 * area
 
 def create_convex_map(X, Y):
-    array = []
+    xy_lis = []
     for x, y in zip(X, Y):
-        array.append([x,y])
-    array = np.array(array)
-    hull = ConvexHull(array)
-    plt.scatter(X, Y, label="key points")
+        xy_lis.append([x,y])
+    xy_lis = np.array(xy_lis)
+    hull = ConvexHull(xy_lis)
+
+    #外周部分の座標配列
+    # x, y = [], []
+    # simplex:array配列の結合部分のindex番号
+    # for simplex, vertice in zip(hull.simplices, hull.vertices):
+    #     x.append(xy_lis[vertice][0])
+    #     y.append(xy_lis[vertice][1])
+    #     plt.plot(xy_lis[simplex, 0], xy_lis[simplex, 1], c="green")
+
+
+    return hull.simplices, hull.vertices
+
+def plot_3d(x, y, z):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_title("3D plot")
+    ax.set_xlabel("x [cm]")
+    ax.set_ylabel("y [cm]")
+    ax.set_zlabel("z [cm]")
+    ax.scatter(x, y, z, marker="^")
+
+def plot_2d(x, y, simplices=None, vertices=None):
+    fig = plt.figure()
+    plt.scatter(x, y, label="key points")
     plt.xlabel("X [cm]")
     plt.ylabel("Y [cm]")
     plt.title("area ")
-    #外周部分の座標配列
-    x, y = [], []
-    # simplex:array配列の結合部分のindex番号
-    for simplex, vertice in zip(hull.simplices, hull.vertices):
-        x.append(array[vertice][0])
-        y.append(array[vertice][1])
-        plt.plot(array[simplex, 0], array[simplex, 1], c="green")
+    for simplex, vertice in zip(simplices, vertices):
+        plt.plot(x[simplex], y[simplex], c="green")
     print(f"area : {polygon_area(x, y)} [cm]")
-
-    plt.legend()
-    # plt.show()
 
 if __name__ == "__main__":
     X_color_M, uv_color = get_X_matrix_and_uv_list("color")
@@ -48,26 +63,20 @@ if __name__ == "__main__":
 
     data = pd.read_csv("./data/pair_plot_position/all_keypoints.txt", header=None)
     xyz3s = np.load('./data/np_xyz.npy')
-    x, y, z = xyz3s[:,0] , xyz3s[:,1], xyz3s[:,2] # kinectから読み込み m単位
+    x_kin, y_kin, z_kin = xyz3s[:,0] * 100 , xyz3s[:,1] * 100, xyz3s[:,2] * 100 # kinectから読み込み cm→m単位
 
-    X, Y, Z = [], [], []
+    X_dlt, Y_dlt, Z_dlt = [], [], []
     for _, content in data.iterrows(): #dltから読み込み　cm単位
         position = list(map(int, content[0].split(" ")))
         x, y, z = get_3d_points(L1, L2, position[:2], position[2:4])
-        X.append(x)
-        Y.append(y)
-        Z.append(-z)
+        X_dlt.append(x)
+        Y_dlt.append(y)
+        Z_dlt.append(-z)
 
-    # create_convex_map(X, Y)#DLTから算出したもの
-    create_convex_map(x, y)#Kinectから
+    # plot_3d(X_dlt, Y_dlt, Z_dlt)
+    plot_3d(x_kin, y_kin, z_kin)
+    sim, con = create_convex_map(x_kin, y_kin)
+    # create_convex_map(X_dlt, Y_dlt)#DLTから算出したもの
+    plot_2d(x_kin, y_kin, sim, con)#Kinectから
 
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.set_title("3D plot")
-    ax.set_xlabel("x [cm]")
-    ax.set_ylabel("y [cm]")
-    ax.set_zlabel("z [cm]")
-
-    # ax.scatter(X, Y, Z, marker="^")#DLT
-    ax.scatter(x, y, z, marker="^")#Kinect
     plt.show()
